0cf76cf7d9eeda94e40af046ecc82c6995b2b56e
Lazar Hristov <lhristov@gmail.com>
2020-11-13 21:51:06 +0200

    Reset to d2cfa9ec

--- tabbed/config.mk
+++ tabbed/config.mk
@@ -12,8 +12,12 @@ X11LIB = /usr/X11R6/lib
 
 # freetype
 FREETYPELIBS = -lfontconfig -lXft
+FREETYPEINC = /usr/include/freetype2
+# OpenBSD (uncomment)
+#FREETYPEINC = ${X11INC}/freetype2
+
 # includes and libs
-INCS = -I. -I/usr/include -I$(X11INC) -I${FREETYPEINC} 
+INCS = -I. -I/usr/include -I$(X11INC) -I${FREETYPEINC}
 LIBS = -L/usr/lib -lc -L${X11LIB} -lX11 ${FREETYPELIBS}
 
 # flags
